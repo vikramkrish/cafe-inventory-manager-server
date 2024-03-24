30608e8c1ec9af09333490728bd4e628267b406c
Vikram V B <vkrm1310@gmail.com>
2024-03-24 15:41:52 +0530

    Vanilla files commit

---
+++ src/main/java/com/cafe/inventory/manager/server/CafeInventoryManagerServerApplication.java
@@ -0,0 +1,13 @@
+package com.cafe.inventory.manager.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeInventoryManagerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeInventoryManagerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cafe/inventory/manager/server/CafeInventoryManagerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafe.inventory.manager.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeInventoryManagerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
