d11ce3290976a4ac5eacd0f533c1451ee72b6e4b
Vikram V B <vkrm1310@gmail.com>
2024-08-15 14:56:15 +0530

    Fixed the empty user population

--- src/main/java/com/cafe/inventory/manager/server/entity/User.java
+++ src/main/java/com/cafe/inventory/manager/server/entity/User.java
@@ -18,6 +18,31 @@ public class User {
     private String email;
 
     // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
 
 
